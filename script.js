997411849bc2da3750143086a1af3431c2ea05aa
Kleberbfabro <bruno.4and@gmaik.com>
2025-03-08 16:12:01 -0300

    08_03_2025_V8

--- script.js
+++ script.js
@@ -63,17 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Verifica se é um dispositivo móvel e retrai a sidebar
   if (window.innerWidth < 768) {
-   // sidebar.classList.add("close", "hoverable");
-   sidebar.classList.add("close");
-    sidebar.classList.remove("hoverable"); // Garante que ela não fique travada fechada
-
+    sidebar.classList.add("close", "hoverable");
+    
   }
 
   //////////////////////////////////////////////////////////
   // Alternar sidebar ao clicar no ícone do menu
-  sidebarOpen.addEventListener("click", () => {
-    sidebar.classList.remove("close"); // Expande a barra lateral
-  });
+ // sidebarOpen.addEventListener("click", () => {
+ //   sidebar.classList.remove("close"); // Expande a barra lateral
+ // });
 
   sidebarExpand.addEventListener("click", () => {
     sidebar.classList.remove("close", "hoverable");
