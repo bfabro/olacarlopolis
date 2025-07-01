4faf3c838baa1a1618a51e13c5ae3ad51a65f398
Kleberbfabro <bruno.4and@gmaik.com>
2025-07-01 19:48:42 -0300

    01_07_25_5

--- script.js
+++ script.js
@@ -7592,7 +7592,7 @@ ${!establishment.descricaoFalecido ? `
                                     <div class="swiper-wrapper">
                                         ${establishment.menuImages.map((img, index) => `
                                         <div class="swiper-slide">
-                                            <img src="${img}" alt="Cardápio ${index + 1} de ${establishment.name}" loading="lazy" decoding="async">
+                                            <img src="${img}" alt="Cardápio ${index + 1} de ${establishment.name}" >
                                         </div>
                                         `).join('')}
                                     </div>
