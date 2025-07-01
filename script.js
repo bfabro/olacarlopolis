1933ce9e23aece59d691022162c421944035213e
Kleberbfabro <bruno.4and@gmaik.com>
2025-07-01 12:42:08 -0300

    01_07_25_3

--- script.js
+++ script.js
@@ -4737,7 +4737,7 @@ image: "images/comercios/despachante/rodriguinho/perfil.png",
               },
 
 
-{
+          {
               image: "images/informacoes/eventos/espacoBrincar.jpg",
               name: "Colonia de Ferias",
               date: "10/07/2025 a 25/07/2025",
@@ -4745,9 +4745,7 @@ image: "images/comercios/despachante/rodriguinho/perfil.png",
               infoAdicional:"De 10 a 25 de julho, o Espaço do Brincar está preparando dias incríveis de muita diversão, aprendizado e carinho para as crianças!<br>Nossa Colônia de Férias foi pensada com todo amor para oferecer:<br> ✨ Atividades lúdicas e Oficinas criativas <br>✨Brincadeiras dirigidas <br>✨Lanchinhos gostosos<br>✨Um ambiente acolhedor e seguro!<br>Temos pacotes especiais para atender à sua rotina:<br>- Meio período ou integral<br>- Diárias avulsas<br>- Planos semanais e quinzenais <br>👉🏻Garanta a vaga do seu pequeno e proporcione momentos inesquecíveis nessas férias!",
               contact: "(43) 99983-8897 ",
               contact2: "(43) 98814-2008 ",
-              instagram:"https://www.instagram.com/espacodobrincareserfeliz/",
-
-              
+              instagram:"https://www.instagram.com/espacodobrincareserfeliz/",             
               
              
           },
@@ -4788,7 +4786,7 @@ image: "images/comercios/despachante/rodriguinho/perfil.png",
               },
 
 
-               {
+            {
               image: "images/informacoes/eventos/carroAntigo.png",
               name: "Encontro Carro Antigo",
               date: "20/07/2025",
@@ -4798,7 +4796,7 @@ image: "images/comercios/despachante/rodriguinho/perfil.png",
               
               
              
-          },
+             },
 
                 {
                   image: "images/informacoes/eventos/senhorbomjesus.png",
