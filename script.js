ad406aaf862ae4242ef2b15073b97c7cf136644e
Kleberbfabro <bruno.4and@gmaik.com>
2025-11-29 10:04:02 -0300

    29_11_25_8

--- script.js
+++ script.js
@@ -4841,7 +4841,7 @@ ${(est.cardapioLink || (est.menuImages && est.menuImages.length) || est.contact)
       codRef: "C_015",
       tipo: "venda",
       procura: "casa", 
-      titulo: "Casa à venda no Residencial Novo Horizonte III",
+      titulo: "Casa no Residencial Novo Horizonte III",
       quartos: 2,
       valor: 215000,
       piscina: false,
