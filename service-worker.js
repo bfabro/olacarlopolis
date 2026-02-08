6b0759915c0e743d47a8d2be247118efc31c47f9
Kleberbfabro <bruno.4and@gmaik.com>
2026-02-08 01:08:26 -0300

    08_02_26_v8

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* service-worker.js */
-const CACHE_VERSION = '2026-02-08-1'; // <-- TROQUE quando publicar atualização
+const CACHE_VERSION = '2026-02-08-v2'; // <-- TROQUE quando publicar atualização
 const CACHE_NAME = `olacarlopolis-${CACHE_VERSION}`;
 
 const PRECACHE_ASSETS = [
