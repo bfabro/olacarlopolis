c6f657ed396a40768dad49205a5fcc3ab7362333
Kleberbfabro <bruno.4and@gmaik.com>
2026-02-18 23:27:52 -0300

    18_02_26_v9

--- script.js
+++ script.js
@@ -10032,7 +10032,7 @@ function sendPaymentReminder(establishment) {
 ///////////////
 
 {
-            image: "images/comercios/lanchonete/noponto/perfil.jpg",
+            image: "images/comercios/lanchonete/noponto/perfil.png",
             name: "No Ponto",
             hours: "Qua a Seg: 19:00h as 23:00h",
             statusAberto: ".",
