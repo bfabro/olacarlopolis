b8f5ab3431c56292815427768e6759ccf64f405b
Kleberbfabro <bruno.4and@gmaik.com>
2026-06-28 10:23:15 -0300

    28_06_26_v15

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* service-worker.js */
-const CACHE_VERSION = '2026-06-28-jogos-rank-promos-eventos-v343'; // atualize ao publicar uma nova versao
+const CACHE_VERSION = '2026-06-28-promocoes-mobile-sem-corte-v344'; // atualize ao publicar uma nova versao
 const CACHE_NAME = `olacarlopolis-${CACHE_VERSION}`;
 
 const PRECACHE_ASSETS = [
